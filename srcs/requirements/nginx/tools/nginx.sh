869b74b0575f1ed1e1475ef2153d9797c4769239
eralonso <eralonso@baltes-g.myguest.virtualbox.org>
2023-10-10 13:22:04 +0200

    Inception - [ nginx: Finsih sript and init config file ] - [ docker-compose.yml -> Added volumes and network ]

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
 if [ ! -f /etc/ssl/certs/nginx.cert ]
-	openssl req -x509 -newkey rsa:4096 -days 365 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.cert -subj "/C=ES/ST=Barcelona/L=Barcelona/O=42/OU=Education/CN=eralonso.42.fr"
+	openssl req -x509 -nodes -newkey rsa:4096 -days 365 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.cert -subj "/C=ES/ST=Barcelona/L=Barcelona/O=42/OU=Education/CN=eralonso.42.fr"
 fi
+
+exec "$@"
