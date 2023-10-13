b24260903abf0e1ae2fe9413504b5ecd07e9b9a6
eralonso <eralonso@baltes-g.myguest.virtualbox.org>
2023-10-13 14:58:44 +0200

    Inception - [nginx: Apparently finish] / [ wordpress: Config file finish and starting script to config wp-config.php ]

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-if [ ! -f /etc/ssl/certs/nginx.cert ]
+if [ ! -f /etc/ssl/certs/nginx.cert ]; then
 	openssl req -x509 -nodes -newkey rsa:4096 -days 365 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.cert -subj "/C=ES/ST=Barcelona/L=Barcelona/O=42/OU=Education/CN=eralonso.42.fr"
 fi
 
