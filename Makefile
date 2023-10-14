97c6dc336da046f6032d9c9f21ce72050d423907
eralonso <eralonso@baltes-g.baltes-g>
2023-10-14 10:20:17 -0400

    Inception - Apparently config is finish

--- Makefile
+++ Makefile
@@ -4,14 +4,14 @@ DATA_DIR := $(addprefix $(HOME),data)
 DATA := wordpress_cont wordpress_db
 DATA := $(addprefix $(DATA_DIR)/,$(DATA))
 
+all:
+	docker-compose -f ./srcs/docker-compose.yml up -d --build
+
 $(DATA_DIR)/%:
 	@mkdir -p $@
 
-all: $(DATA)
-	@docker compose -f srcs/docker-compose.yml up -d --build
-
-down:
-	@docker compose -f srcs/docker-compose.yml down
+own:
+	@docker-compose -f ./srcs/docker-compose.yml down
 
 clean: down
 	@docker stop $$(docker ps -qa)
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -4,7 +4,7 @@ if [ ! -f wp-config.php ]; then
 	wp core download --allow-root 
 	wp config create --dbname=$DATA_BASE_NAME --dbuser=$DATA_BASE_USER --dbpass=$DATA_BASE_PASSWORD --dbhost=$DATA_BASE_HOSTNAME --allow-root
 	wp core install --url=$DOMAIN_NAME --title="$WORDPRESS_TITLE" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email --allow-root
-	wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user-pass=$WORDPRESS_USER_PASS --allow-root
+	wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user-pass=$WORDPRESS_USER_PASSWORD --allow-root
 	wp theme install twentytwentytwo --allow-root
 fi
 
